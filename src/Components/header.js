31322228608ea5998ef3ea9ad040b1a4956901ba
Eric Trimble <eric.trimo@gmail.com>
2021-01-16 14:03:38 -0500

    install ant design

---
+++ src/Components/header.js
@@ -0,0 +1,14 @@
+import '../App.css';
+
+// https://ant.design/components/menu/
+
+function header() {
+  <Menu>
+    <Menu.Item>Menu</Menu.Item>
+    <SubMenu title="SubMenu">
+      <Menu.Item>SubMenuItem</Menu.Item>
+    </SubMenu>
+  </Menu>
+}
+
+export default Header;
